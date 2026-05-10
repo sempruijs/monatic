3618abaf26142b01ee34e7974a95948e52af9aa2
Sem Pruijs <sem@pruijs.net>
2026-05-10 11:41:41 +0200

    Initial Commit

---
+++ linker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  linker
+//
+//  Created by Sem Pruijs on 10/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ linker/linkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  linkerApp.swift
+//  linker
+//
+//  Created by Sem Pruijs on 10/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct linkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
